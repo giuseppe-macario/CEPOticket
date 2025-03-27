e60e7e66f1f3737d127ff80c7bd067786cff4401
Giuseppe Macario <141560720+giuseppe-macario@users.noreply.github.com>
2025-03-27 10:28:51 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "askgpt-cache-v1";
+const OFFLINE_FILES = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/main.js",
+  "/js/speech.js",
+  "/js/gpt.js",
+  "/js/utils.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
